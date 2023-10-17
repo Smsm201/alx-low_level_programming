d96b5442eb1d98d63635cc3d61b65362899519b9
smsm201 <drahmedsamir365@gmail.com>
2023-10-17 14:10:55 -0700

    y

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * '_strcpy - copies the string pointed to by str
+ * @dest: chat type string
+ * @src: char type string
+ * description: copy the string pointed to by pointer 'src' to 
+ * the buffer to by 'dest'
+ * Return: 0 for successful execution.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
